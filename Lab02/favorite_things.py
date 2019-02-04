b3131a60e4308b8071e8480aa90319a168c63690
Ian Cowan <ian@cowanemail.com>
2019-02-04 15:55:14 -0500

    Lab02

---
+++ Lab02/favorite_things.py
@@ -0,0 +1,16 @@
+# Ian Cowan
+# Lab Partner: Jake Pfaller
+# CSC 117b Lab02 I/O
+# 04 Feb 2019
+# Favorite Things
+
+# Begin prompting the user
+integer = int(input('What is your favorite integer? '))
+color = input('What is your favorite color? ')
+animal = input('What is your favorite animal? ')
+# End prompting the user
+
+# Begin telling the user their favorite things
+print('Your favorite integer is', integer)
+print('Your favorite color is', color, 'and your favorite animal is the', animal)
+# End telling the user their favorite things
